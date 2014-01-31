a4dd8cd08d76ad0ec6cd690493e0bb1cc5a8b4fb
Philip Horger <campadrenalin@gmail.com>
2014-01-30 18:42:42 -0800

    Split off DocumentFile type for serialization/deserialization

--- document.go
+++ document.go
@@ -1,5 +1,13 @@
 package deje
 
+// A document is a single managed DEJE object, associated with
+// a single immutable IRCLocation, and self-describing its
+// actions and permissions as part of the content.
+//
+// The content of a Document is the result of applying the
+// "official" chain of history, in much the same way that the
+// Bitcoin ledger is the result of playing through the transactions
+// in every block of the longest valid blockchain.
 type Document struct {
 	Channel    IRCLocation
 	Events     EventSet
@@ -7,9 +15,37 @@ type Document struct {
 	Timestamps TimestampSet `json:""`
 }
 
+// Used for serializing and deserializing docs to files.
+//
+// This allows us to use more complicated structures for actual
+// documents, that allow for storing Timestamps, and other data
+// that we must not trust the file to provide.
+type DocumentFile struct {
+	Channel IRCLocation
+	Events  EventSet
+	Syncs   SyncSet
+}
+
+// Create a new, blank Document, with fields initialized.
 func NewDocument() Document {
 	return Document{
 		Events: make(EventSet),
 		Syncs:  make(SyncSet),
+	}
+}
+
+// Copies the data from a DocumentFile into a Document.
+func (d *Document) FromFile(df *DocumentFile) {
+	d.Channel = df.Channel
+	d.Events = df.Events
+	d.Syncs = df.Syncs
+}
+
+// Copies the data from a Document into a DocumentFile.
+func (d *Document) ToFile() *DocumentFile {
+	return &DocumentFile{
+		Channel: d.Channel,
+		Events:  d.Events,
+		Syncs:   d.Syncs,
 	}
 }
