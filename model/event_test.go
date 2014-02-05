7fed81fcdceffb00d0d2d7cfbf186a236b8f1d5d
Philip Horger <campadrenalin@gmail.com>
2014-02-04 17:57:34 -0800

    Improve test coverage - will update for OM later

--- model/event_test.go
+++ model/event_test.go
@@ -37,3 +37,29 @@ func TestEventSet_GetRoot(t *testing.T) {
 		}
 	}
 }
+
+func TestEventSetContains(t *testing.T) {
+	set := make(EventSet)
+	first := NewEvent("first")
+	second := NewEvent("second")
+	third := NewEvent("third")
+
+	second.SetParent(first)
+	third.SetParent(second)
+
+	events := []Event{first, third} // Every event but second
+	for _, ev := range events {
+		set.Register(ev)
+	}
+
+    if ! set.Contains(first) {
+        t.Fatal("set should contain first")
+    }
+    if ! set.Contains(third) {
+        t.Fatal("set should contain third")
+    }
+
+    if set.Contains(second) {
+        t.Fatal("set should contain second")
+    }
+}
