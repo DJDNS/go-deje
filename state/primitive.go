458926302ee4b012f01f94a2d8fb27183d3acbe0
Philip Horger <campadrenalin@gmail.com>
2014-03-07 21:00:14 -0800

    Fully implemented primitives

--- state/primitive.go
+++ state/primitive.go
@@ -1,8 +1,28 @@
 package state
 
+import "errors"
+
 type Primitive interface {
 	Apply(*DocumentState) error
 	Reverse(*DocumentState) (Primitive, error)
+}
+
+// Given a root and a path (call the pointed-to Container X),
+// return X's parent, and the key from the parent to X.
+//
+// This is used throughout the primitives, so it makes sense
+// to implement it as common code.
+func getTraversal(c Container, path []interface{}) (Container, interface{}, error) {
+	if len(path) == 0 {
+		return nil, nil, errors.New("Empty path - must have >= 1 key")
+	}
+	traversal, last := path[:len(path)-1], path[len(path)-1]
+	parent, err := Traverse(c, traversal)
+	if err != nil {
+		return nil, nil, err
+	} else {
+		return parent, last, nil
+	}
 }
 
 // Corresponds to SET builtin event handler.
@@ -21,11 +41,42 @@ func (p *SetPrimitive) Apply(ds *DocumentState) error {
 		return nil
 	}
 
-	traversal := p.Path[:len(p.Path)-1]
-	last := p.Path[len(p.Path)-1]
-	parent, err := Traverse(ds.Value, traversal)
+	parent, last, err := getTraversal(ds.Value, p.Path)
 	if err != nil {
 		return err
 	}
 	return parent.Set(last, p.Value)
+}
+
+func (p *SetPrimitive) Reverse(ds *DocumentState) (Primitive, error) {
+	primitive := &SetPrimitive{
+		Path:  []interface{}{},
+		Value: ds.Export(),
+	}
+	return primitive, nil
+}
+
+// Corresponds to DELETE builtin event handler.
+type DeletePrimitive struct {
+	Path []interface{}
+}
+
+func (p *DeletePrimitive) Apply(ds *DocumentState) error {
+	if len(p.Path) == 0 {
+		return errors.New("Cannot delete root node")
+	}
+
+	parent, last, err := getTraversal(ds.Value, p.Path)
+	if err != nil {
+		return err
+	}
+	return parent.RemoveChild(last)
+}
+
+func (p *DeletePrimitive) Reverse(ds *DocumentState) (Primitive, error) {
+	primitive := &SetPrimitive{
+		Path:  []interface{}{},
+		Value: ds.Export(),
+	}
+	return primitive, nil
 }
--- state/primitive_test.go
+++ state/primitive_test.go
@@ -5,6 +5,73 @@ import (
 	"testing"
 )
 
+type primitiveApplyTest struct {
+	Change     Primitive
+	Expected   interface{}
+	FailureMsg string
+}
+
+func TestGetTraversal(t *testing.T) {
+	original := map[string]interface{}{
+		"hello": "world",
+		"deep": map[string]interface{}{
+			"stuff": "in here",
+		},
+	}
+	container, err := MakeContainer(original)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, _, err = getTraversal(container, []interface{}{})
+	if err == nil {
+		t.Fatal("getTraversal should fail for zero-length path")
+	}
+
+	_, _, err = getTraversal(container, []interface{}{"this", "that"})
+	if err == nil {
+		t.Fatal("getTraversal should fail for bad traversal")
+	}
+
+	p, l, err := getTraversal(container, []interface{}{"this"})
+	if err != nil {
+		t.Error("getTraversal should not fail for nonexistent last")
+		t.Fatal(err)
+	}
+	expected := original
+	if !reflect.DeepEqual(p.Export(), expected) {
+		t.Fatalf("Expected %#v, got %#v", expected, p.Export())
+	}
+	if l != "this" {
+		t.Fatalf("Expected %#v, got %#v", "this", l)
+	}
+
+	p, l, err = getTraversal(container, []interface{}{"deep", "stuff"})
+	if err != nil {
+		t.Error("getTraversal should not fail for deep traversal")
+		t.Fatal(err)
+	}
+	expected = map[string]interface{}{"stuff": "in here"}
+	if !reflect.DeepEqual(p.Export(), expected) {
+		t.Fatalf("Expected %#v, got %#v", expected, p.Export())
+	}
+	if l != "stuff" {
+		t.Fatalf("Expected %#v, got %#v", "this", l)
+	}
+}
+
+func (test *primitiveApplyTest) Run(t *testing.T, ds *DocumentState) {
+	err := test.Change.Apply(ds)
+	if err != nil {
+		t.Fatal(err)
+	}
+	exported := ds.Export()
+	if !reflect.DeepEqual(exported, test.Expected) {
+		t.Error(test.FailureMsg)
+		t.Fatalf("Expected %#v, got %#v", test.Expected, exported)
+	}
+}
+
 func TestSetPrimitive_Apply_Root(t *testing.T) {
 	ds := NewDocumentState()
 	primitive := SetPrimitive{
@@ -83,5 +150,207 @@ func TestSetPrimitive_Apply_WithPath(t *testing.T) {
 	exported := ds.Export()
 	if !reflect.DeepEqual(exported, expected) {
 		t.Fatalf("Expected %#v, got %#v", expected, exported)
+	}
+}
+
+func TestDeletePrimitive_Apply_Root(t *testing.T) {
+	ds := NewDocumentState()
+	primitive := DeletePrimitive{
+		Path: []interface{}{},
+	}
+	err := primitive.Apply(ds)
+	if err == nil {
+		t.Fatal("DeletePrimitive.Apply should always fail for root")
+	}
+}
+
+func TestDeletePrimitive_Apply_WithPath(t *testing.T) {
+	ds := NewDocumentState()
+	primitive := DeletePrimitive{
+		Path: []interface{}{"this", "that"},
+	}
+	err := primitive.Apply(ds)
+	if err == nil {
+		t.Fatal("DeletePrimitive.Apply should fail for bad traversals")
+	}
+	primitive.Path = []interface{}{"this"}
+	err = primitive.Apply(ds)
+	if err != nil {
+		t.Fatal(err) // Should not fail - deletion is idempotent
+	}
+
+	// Confirm that actual deletion happens
+	setter := SetPrimitive{
+		Path: []interface{}{},
+		Value: map[string]interface{}{
+			"hello": "world",
+			"other": "stuff",
+			"deep": map[string]interface{}{
+				"this":   "that",
+				"things": []interface{}{true, false, nil},
+			},
+		},
+	}
+	err = setter.Apply(ds)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []primitiveApplyTest{
+		primitiveApplyTest{
+			&DeletePrimitive{[]interface{}{"this"}},
+			map[string]interface{}{
+				"hello": "world",
+				"other": "stuff",
+				"deep": map[string]interface{}{
+					"this":   "that",
+					"things": []interface{}{true, false, nil},
+				},
+			},
+			"Deleting nonexistent key should have no effect",
+		},
+		primitiveApplyTest{
+			&DeletePrimitive{[]interface{}{"other"}},
+			map[string]interface{}{
+				"hello": "world",
+				"deep": map[string]interface{}{
+					"this":   "that",
+					"things": []interface{}{true, false, nil},
+				},
+			},
+			"Should have removed 'other' item from DS",
+		},
+		primitiveApplyTest{
+			&DeletePrimitive{[]interface{}{"deep", "things", 1}},
+			map[string]interface{}{
+				"hello": "world",
+				"deep": map[string]interface{}{
+					"this":   "that",
+					"things": []interface{}{true, nil},
+				},
+			},
+			"Should have removed deep-traversal item from DS",
+		},
+	}
+	for _, test := range tests {
+		test.Run(t, ds)
+	}
+}
+
+type primitiveReverseTest struct {
+	Original   interface{}
+	Change     Primitive
+	FailureMsg string
+}
+
+func (test *primitiveReverseTest) Run(t *testing.T) {
+	ds := NewDocumentState()
+	setter := SetPrimitive{
+		Path:  []interface{}{},
+		Value: test.Original,
+	}
+	err := setter.Apply(ds)
+	if err != nil {
+		t.Error(test.FailureMsg)
+		t.Fatal(err)
+	}
+
+	reverse, err := test.Change.Reverse(ds)
+	if err != nil {
+		t.Error(test.FailureMsg)
+		t.Fatal(err)
+	}
+
+	err = test.Change.Apply(ds)
+	if err != nil {
+		t.Error(test.FailureMsg)
+		t.Fatal(err)
+	}
+
+	err = reverse.Apply(ds)
+	if err != nil {
+		t.Error(test.FailureMsg)
+		t.Fatal(err)
+	}
+	exported := ds.Export()
+	if !reflect.DeepEqual(exported, test.Original) {
+		t.Error(test.FailureMsg)
+		t.Fatalf("Expected %#v, got %#v", test.Original, exported)
+	}
+}
+
+func TestSetPrimitive_Reverse(t *testing.T) {
+	tests := []primitiveReverseTest{
+		primitiveReverseTest{
+			Original: map[string]interface{}{},
+			Change: &SetPrimitive{
+				Path: []interface{}{},
+				Value: map[string]interface{}{
+					"hello": "world",
+				},
+			},
+			FailureMsg: "Reversal on root change",
+		},
+		primitiveReverseTest{
+			Original: map[string]interface{}{
+				"existing": "stuff",
+			},
+			Change: &SetPrimitive{
+				Path: []interface{}{},
+				Value: map[string]interface{}{
+					"hello": "world",
+				},
+			},
+			FailureMsg: "Reversal on root restores old root",
+		},
+		primitiveReverseTest{
+			Original: map[string]interface{}{
+				"existing": "stuff",
+			},
+			Change: &SetPrimitive{
+				Path:  []interface{}{"hello"},
+				Value: "world",
+			},
+			FailureMsg: "Reversal on root does not affect existing",
+		},
+	}
+
+	for _, test := range tests {
+		test.Run(t)
+	}
+}
+
+func TestDeletePrimitive_Reverse(t *testing.T) {
+	tests := []primitiveReverseTest{
+		primitiveReverseTest{
+			Original: map[string]interface{}{},
+			Change: &DeletePrimitive{
+				Path: []interface{}{"missing"},
+			},
+			FailureMsg: "Reversal on root change",
+		},
+		primitiveReverseTest{
+			Original: map[string]interface{}{
+				"existing": "stuff",
+			},
+			Change: &DeletePrimitive{
+				Path: []interface{}{"existing"},
+			},
+			FailureMsg: "Reversal on root restores old root",
+		},
+		primitiveReverseTest{
+			Original: map[string]interface{}{
+				"existing": "stuff",
+				"hello":    "world",
+			},
+			Change: &DeletePrimitive{
+				Path: []interface{}{"hello"},
+			},
+			FailureMsg: "Reversal on root does not affect existing",
+		},
+	}
+
+	for _, test := range tests {
+		test.Run(t)
 	}
 }
