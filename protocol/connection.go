f0eb8ccf3b466070f9c4b91ed7376b2fcf8aa76c
Philip Horger <campadrenalin@gmail.com>
2014-03-23 19:46:54 -0700

    conn.onEvent

--- protocol/connection.go
+++ protocol/connection.go
@@ -8,11 +8,12 @@ import (
 )
 
 type Connection struct {
-	Channel services.IRCChannel
+	Document logic.Document
+	Channel  services.IRCChannel
 }
 
 func NewConnection(d logic.Document, c services.IRCChannel) Connection {
-	return Connection{c}
+	return Connection{d, c}
 }
 
 func (p Connection) PublishEvent(ev model.Event) error {
@@ -21,5 +22,16 @@ func (p Connection) PublishEvent(ev model.Event) error {
 		return err
 	}
 	p.Channel.Outgoing <- "deje event " + string(marshalled)
+	return nil
+}
+
+func (p Connection) onEvent(evstr string) error {
+	var ev model.Event
+	err := json.Unmarshal([]byte(evstr), &ev)
+	if err != nil {
+		return err
+	}
+	event := logic.Event{ev, &p.Document}
+	event.Register()
 	return nil
 }
--- protocol/connection_test.go
+++ protocol/connection_test.go
@@ -51,3 +51,86 @@ func TestConnection_PublishEvent_Unserializable(t *testing.T) {
 		t.Fatalf("Expected 0 items in output, got %d", len(c.Outgoing))
 	}
 }
+
+type ParseEventTest struct {
+	Name          string
+	Input         string
+	Event         model.Event
+	ShouldSucceed bool
+}
+
+func (test ParseEventTest) Run(t *testing.T) {
+	d := logic.NewDocument()
+	c := services.NewIRCChannel(location)
+	conn := NewConnection(d, c)
+
+	err := conn.onEvent(test.Input)
+	was_registered := d.Events.Contains(test.Event)
+	if test.ShouldSucceed {
+		if err != nil {
+			t.Log(test.Name)
+			t.Fatal(err)
+		}
+		if !was_registered {
+			t.Log(test.Name)
+			t.Error("Event not registered")
+			t.Fatalf("%#v", d.Events.GetItems())
+		}
+	} else {
+		if err == nil {
+			t.Log(test.Name)
+			t.Fatal("Parse should fail, but didn't!")
+		}
+		if was_registered {
+			t.Log(test.Name)
+			t.Fatal("Event was registered")
+		}
+	}
+}
+
+func TestConnection_onEvent(t *testing.T) {
+	ev := model.NewEvent("handler_name")
+	ev.Arguments["hello"] = "world"
+	ev.ParentHash = "mainstream television"
+	tests := []ParseEventTest{
+		ParseEventTest{
+			"basic",
+			`{"parent":"mainstream television",` +
+				`"handler":"some_handler",` +
+				`"args":{"hello":"world"}}`,
+			model.Event{
+				ParentHash:  "mainstream television",
+				HandlerName: "some_handler",
+				Arguments: map[string]interface{}{
+					"hello": "world",
+				},
+			},
+			true,
+		},
+		ParseEventTest{
+			"whitespace",
+			` {"parent":"",  "handler":"foo","args":{}}  `,
+			model.Event{
+				HandlerName: "foo",
+				Arguments:   map[string]interface{}{},
+			},
+			true,
+		},
+		ParseEventTest{
+			"crasher",
+			"crasher",
+			model.Event{},
+			false,
+		},
+		ParseEventTest{
+			"leading protocol crap",
+			`deje event {"parent":"","handler":"","args":{}}`,
+			model.Event{},
+			false,
+		},
+	}
+
+	for _, test := range tests {
+		test.Run(t)
+	}
+}
