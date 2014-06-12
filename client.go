53f8c8a4b04d0ad4fddc651b2c66680087947dc8
Philip Horger <campadrenalin@gmail.com>
2014-06-11 23:22:38 -0700

    Add pubsub test

--- client.go
+++ client.go
@@ -9,6 +9,7 @@ import (
 type Client struct {
 	Doc *logic.Document
 
+	onEvent  *OnEventCallback
 	tpClient *turnpike.Client
 }
 
@@ -21,9 +22,13 @@ func NewClient(topic string) Client {
 	}
 }
 
-// When a session connects, this client is provided with the session
-// ID value. This is just a server-chosen string.
+// When a session connects, this callback is provided with the session
+// ID value. sessionId is just a server-chosen string.
 type OnConnectCallback func(sessionId string)
+
+// Called when another peer in the same WAMP topic publishes a
+// JSON-compatible event to all subscribers.
+type OnEventCallback func(event interface{})
 
 // Set callback to be executed when a successful connection has been
 // made to a WAMP router.
@@ -31,8 +36,32 @@ func (c *Client) SetConnectCallback(callback OnConnectCallback) {
 	c.tpClient.SetSessionOpenCallback(callback)
 }
 
+// Set callback to be executed when a published event is received.
+func (c *Client) SetEventCallback(callback OnEventCallback) {
+	c.onEvent = &callback
+}
+
+// Publish an event to all subscribers. Can be any JSON-compatible
+// value.
+func (c *Client) Publish(event interface{}) error {
+	return c.tpClient.PublishExcludeMe(
+		c.Doc.Topic,
+		event,
+	)
+}
+
 // Connect to a WAMP router. This also calls the 'connect' callback on
 // success.
 func (c *Client) Connect(url string) error {
-	return c.tpClient.Connect(url, "http://localhost/")
+	err := c.tpClient.Connect(url, "http://localhost/")
+	if err != nil {
+		return err
+	}
+
+	handler := func(topic string, event interface{}) {
+		if c.onEvent != nil {
+			(*c.onEvent)(event)
+		}
+	}
+	return c.tpClient.Subscribe(c.Doc.Topic, handler)
 }
--- client_test.go
+++ client_test.go
@@ -3,12 +3,14 @@ package deje
 import (
 	"github.com/jcelliott/turnpike"
 	"net/http/httptest"
+	"reflect"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestClient_NewClient(t *testing.T) {
-	topic := "com.example.deje.some-doc"
+	topic := "http://example.com/deje/some-doc"
 	client := NewClient(topic)
 
 	if client.Doc.Topic != topic {
@@ -32,7 +34,7 @@ func setupServer() (string, func()) {
 }
 
 func TestClient_Connect(t *testing.T) {
-	topic := "com.example.deje.some-doc"
+	topic := "http://example.com/deje/some-doc"
 	client := NewClient(topic)
 	server_addr, server_closer := setupServer()
 	defer server_closer()
@@ -49,7 +51,7 @@ func TestClient_Connect(t *testing.T) {
 }
 
 func TestClient_Connect_WithCallback(t *testing.T) {
-	topic := "com.example.deje.some-doc"
+	topic := "http://example.com/deje/some-doc"
 	client := NewClient(topic)
 	server_addr, server_closer := setupServer()
 	defer server_closer()
@@ -71,4 +73,70 @@ func TestClient_Connect_WithCallback(t *testing.T) {
 	if callback_called_with == "" {
 		t.Fatal("Callback was not given a sessionId value")
 	}
+}
+
+func TestClient_PubSub(t *testing.T) {
+	topic := "http://example.com/deje/some-doc"
+	client1 := NewClient(topic)
+	client2 := NewClient(topic)
+	server_addr, server_closer := setupServer()
+	defer server_closer()
+
+	// Connect both clients
+	if err := client1.Connect(server_addr); err != nil {
+		t.Fatal(err)
+	}
+	if err := client2.Connect(server_addr); err != nil {
+		t.Fatal(err)
+	}
+
+	// Give client1 a subscription callback
+	response_chan := make(chan interface{}, 1)
+	client1.SetEventCallback(func(event interface{}) {
+		response_chan <- event
+	})
+
+	// Publish via client2
+	sent := map[string]interface{}{
+		"foo":  "bar",
+		"fire": []interface{}{"ant", "place", "nation"},
+	}
+	if err := client2.Publish(sent); err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case recvd := <-response_chan:
+		if !reflect.DeepEqual(recvd, sent) {
+			t.Fatalf("Expected %v, got %v", sent, recvd)
+		}
+	case <-time.After(15 * time.Millisecond):
+		t.Fatal("Recv timed out")
+	}
+
+	// Confirm that client2 doesn't break if onEvent == nil
+	client1.Publish(sent)
+
+	// And that client1 does not receive its own events
+	if len(response_chan) > 0 {
+		t.Fatal("client1 recvd response from self")
+	}
+
+	// Finally, confirm that callbacks can be set after the fact
+	client2.SetEventCallback(func(event interface{}) {
+		response_chan <- "I can recv too!"
+	})
+	if err := client1.Publish(sent); err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case recvd := <-response_chan:
+		if !reflect.DeepEqual(recvd, "I can recv too!") {
+			t.Fatalf("Expected %v, got %v", sent, recvd)
+		}
+	case <-time.After(15 * time.Millisecond):
+		t.Fatal("Recv timed out")
+	}
+
 }
