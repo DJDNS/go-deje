3f6db4d1375be08d9463917182423d4345df5a47
Philip Horger <campadrenalin@gmail.com>
2014-06-13 21:52:55 -0700

    #4 Module docs for ./state

---
+++ state/docs.go
@@ -0,0 +1,6 @@
+// Model for the state of a DEJE Document.
+//
+// This contains all the logic for turning events into actionable
+// changes on a DocumentState (these actionable changes are called
+// Primitives), and applying these primitives to the DocumentState.
+package state
