eda414f7df5f51a1def33c2b23db17561d4773b4
Philip Horger <campadrenalin@gmail.com>
2014-06-14 22:33:29 -0700

    #8 Remove obsolete logic and model packages

--- logic/docs.go
+++
@@ -1,9 +0,0 @@
-// DEJE Documents and elements, at a level where everything's registered.
-//
-// It's not quite an apocalypse of Soviet control and despairing
-// dystopia. What it really means is that all the little pieces can
-// discover and observe each other, which allows for more complex
-// behaviors, like Event.GetCommonAncestor.
-//
-// Hopefully the logic and model packages will be able to be merged soon.
-package logic
