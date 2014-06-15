83152ca024a28638fe3f270b31100c2eedcb93d2
Philip Horger <campadrenalin@gmail.com>
2014-06-15 11:12:41 -0700

    #7 Add Serialize support

--- document/document.go
+++ document/document.go
@@ -1,6 +1,10 @@
 package document
 
-import "github.com/campadrenalin/go-deje/state"
+import (
+	"encoding/json"
+	"github.com/campadrenalin/go-deje/state"
+	"io"
+)
 
 // A document is a single managed DEJE object, associated with
 // a single immutable topic string, and self-describing its
@@ -11,8 +15,8 @@ import "github.com/campadrenalin/go-deje/state"
 // Bitcoin ledger is the result of playing through the transactions
 // in every block of the longest valid blockchain.
 type Document struct {
-	Topic string
-	State *state.DocumentState
+	Topic string               `json:"topic"`
+	State *state.DocumentState `json:"-"`
 
 	// Do not modify the contents of the following fields!
 	// They're there for you to have convenient and uninhibited
@@ -21,10 +25,10 @@ type Document struct {
 	//
 	// Please just use the Thing.Register() and Thing.Unregister()
 	// methods, and when it comes to these fields, LOOK BUT DON'T TOUCH.
-	Events         EventSet
-	EventsByParent map[string]EventSet
-	Quorums        QuorumSet
-	QuorumsByEvent map[string]QuorumSet
+	Events         EventSet             `json:"events"`
+	EventsByParent map[string]EventSet  `json:"-"`
+	Quorums        QuorumSet            `json:"quorums"`
+	QuorumsByEvent map[string]QuorumSet `json:"-"`
 }
 
 // Create a new, blank Document, with fields initialized.
@@ -36,4 +40,10 @@ func NewDocument() Document {
 		Quorums:        make(QuorumSet),
 		QuorumsByEvent: make(map[string]QuorumSet),
 	}
+}
+
+// Serialize JSON data to an io.Writer.
+func (d *Document) Serialize(w io.Writer) error {
+	encoder := json.NewEncoder(w)
+	return encoder.Encode(d)
 }
--- document/document_test.go
+++ document/document_test.go
@@ -1,6 +1,8 @@
 package document
 
 import (
+	"bytes"
+	"errors"
 	"testing"
 )
 
@@ -21,5 +23,70 @@ func TestNewDocument(t *testing.T) {
 	}
 	if d.QuorumsByEvent == nil {
 		t.Fatal("d.QuorumsByEvent == nil")
+	}
+}
+
+type BrokenBuffer struct{}
+
+func (bb BrokenBuffer) Read([]byte) (int, error) {
+	return 0, errors.New("BrokenBuffer cannot be read >:)")
+}
+func (bb BrokenBuffer) Write([]byte) (int, error) {
+	return 0, errors.New("BrokenBuffer cannot be written to >:)")
+}
+
+func TestDocument_Serialize_Empty(t *testing.T) {
+	var buffer bytes.Buffer
+	d := NewDocument()
+	if err := d.Serialize(&buffer); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `{"topic":"","events":{},"quorums":{}}` + "\n"
+	got := buffer.String()
+	if got != expected {
+		t.Fatalf("Expected %#v, got %#v", expected, got)
+	}
+}
+
+func TestDocument_Serialize_Broken(t *testing.T) {
+	var buffer BrokenBuffer
+	d := NewDocument()
+	if err := d.Serialize(&buffer); err == nil {
+		t.Fatal("Serialization should fail")
+	}
+}
+
+func TestDocument_Serialize_WithStuff(t *testing.T) {
+	var buffer bytes.Buffer
+	d := NewDocument()
+	d.Topic = "Frolicking"
+
+	ev := d.NewEvent("some handler name")
+	ev.Arguments["arg"] = "value"
+	ev.ParentHash = "Fooblamoose"
+	ev.Register()
+
+	q := d.NewQuorum("some event hash")
+	q.Signatures["brian blessed"] = "BRIAAAN BLESSED!"
+	q.Register()
+
+	if err := d.Serialize(&buffer); err != nil {
+		t.Fatal(err)
+	}
+	expected := `{"topic":"Frolicking",` +
+		`"events":{` +
+		`"` + ev.GetKey() + `":{` +
+		`"parent":"Fooblamoose","handler":"some handler name",` +
+		`"args":{"arg":"value"}` +
+		`}},"quorums":{` +
+		`"` + q.GetKey() + `":{` +
+		`"event_hash":"some event hash",` +
+		`"sigs":{"brian blessed":"BRIAAAN BLESSED!"}` +
+		`}}}` +
+		"\n"
+	got := buffer.String()
+	if got != expected {
+		t.Fatalf("Expected %#v\n\nGot %#v", expected, got)
 	}
 }
