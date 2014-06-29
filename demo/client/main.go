b7e68c499074264daac40fff26b43470a3e61b1e
Philip Horger <campadrenalin@gmail.com>
2014-06-29 13:43:01 -0700

    Add clients to demo

---
+++ demo/client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/campadrenalin/go-deje"
+	"log"
+	"os"
+	"os/signal"
+)
+
+func main() {
+    url := "ws://localhost:8080/ws"
+    topic := "http://demo/"
+	logger := log.New(os.Stderr, "deje.SimpleClient: ", 0)
+
+	sc := deje.NewSimpleClient(topic, logger)
+    if err := sc.Connect(url); err != nil {
+        logger.Fatal(err)
+    } else {
+        logger.Println("Connected to " + url)
+        logger.Printf("Listening to topic '%s'", topic)
+    }
+
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, os.Interrupt)
+    <-c
+    log.Println("Shutting down client")
+}
