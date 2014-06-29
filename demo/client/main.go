b6cfd3f34e0d8a371bebb36de92413749fc833dd
Philip Horger <campadrenalin@gmail.com>
2014-06-29 14:30:06 -0700

    Fix fmting of demo client

--- demo/client/main.go
+++ demo/client/main.go
@@ -8,20 +8,20 @@ import (
 )
 
 func main() {
-    url := "ws://localhost:8080/ws"
-    topic := "http://demo/"
+	url := "ws://localhost:8080/ws"
+	topic := "http://demo/"
 	logger := log.New(os.Stderr, "deje.SimpleClient: ", 0)
 
 	sc := deje.NewSimpleClient(topic, logger)
-    if err := sc.Connect(url); err != nil {
-        logger.Fatal(err)
-    } else {
-        logger.Println("Connected to " + url)
-        logger.Printf("Listening to topic '%s'", topic)
-    }
+	if err := sc.Connect(url); err != nil {
+		logger.Fatal(err)
+	} else {
+		logger.Println("Connected to " + url)
+		logger.Printf("Listening to topic '%s'", topic)
+	}
 
-    c := make(chan os.Signal, 1)
-    signal.Notify(c, os.Interrupt)
-    <-c
-    log.Println("Shutting down client")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	log.Println("Shutting down client")
 }
