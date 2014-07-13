13e05edc7679df32d96523eedd8100fbf8d7721d
Philip Horger <campadrenalin@gmail.com>
2014-07-13 08:57:57 -0700

    #24 Use correct error msg and print usage for no_args

--- djconvert/main.go
+++ djconvert/main.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/DJDNS/go-deje/document"
 )
 
+var usage_string = `
+USAGE:
+
+    # Convert snapshot to flat-history document cache
+    djconvert up original.json deje.json
+    djconvert up original.json deje.json --pretty
+
+    # Export event in document cache to snapshot
+    djconvert down deje.json snapshot.json 89efc6
+    djconvert down deje.json snapshot.json 89efc6 --pretty
+`
+
+func usage() {
+	fmt.Fprint(os.Stderr, usage_string)
+	os.Exit(1)
+}
+
 func main() {
 	log.SetFlags(0)
 	log.SetPrefix("djconvert: ")
+	args := flag.Args()
+
+	if len(args) < 1 {
+		log.Println("Not enough args, need at least a subcommand 'up' or 'down'")
+		usage()
+	}
 
 	doc := document.NewDocument()
 	input_filename := "input.json"
@@ -39,5 +64,5 @@ func main() {
 	if err := doc.Serialize(output); err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("Successfully wrote %s", output_filename)
+	log.Printf("Successfully wrote %s\n", output_filename)
 }
